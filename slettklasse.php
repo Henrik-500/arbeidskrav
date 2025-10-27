1018f4f070379e8a3e82663c2f8e2e676be769c3
Henrik-500 <henrikkenson@gmail.com>
2025-10-27 03:10:27 +0100

    25

--- slettklasse.php
+++ slettklasse.php
@@ -61,7 +61,7 @@
               mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
                 /* SQL-setning sendt til database-serveren */
 		
-              print ("F&oslash;lgende klasse er n&aring; slettet: $klassekode $klassenavn  <br />");
+              print ("F&oslash;lgende klasse er n&aring; slettet: $klassekode $klassenavn <br />");
             }
         }
     }
